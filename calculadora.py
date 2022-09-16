7dad738d6b9f94ab2acf3a885a1a4cf74037cac4
Samuel K Africano <Kandumboafricano@gmail.com>
2022-09-16 14:03:33 +1200

    Actulizando o arquivo, para compatibilizar com o PEP8

--- calculadora.py
+++ calculadora.py
@@ -2,10 +2,11 @@
 Este modulo contêm as oprações básicas de uma calculadora.
 """
 
+
 def somar(num_1, num_2):
     return num_1 + num_2
 
-def subtrair(num_1, num_2):
-    return num_1 - num_2
 
-    
+def subtrair(num_1, num_2):    
+    return num_1 - num_2 
+       
